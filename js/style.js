b086b150b81db24f0b5ae71114647d3e84f5fade
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-03-24 10:15:57 +0545

    javaScript Added

---
+++ js/style.js
@@ -0,0 +1,5 @@
+/* ===============  toggler-style-switcher  ===============*/ 
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
